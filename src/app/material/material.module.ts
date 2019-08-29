75be8f3405a7b6c11f4fe61427450b208c448591
ZIN MYO <overmidnight12@gmail.com>
2019-08-28 23:03:53 -0700

    my changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule, MatButtonToggleModule, MatIconModule, MatProgressSpinnerModule, MatToolbarModule} from '@angular/material';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {FormsModule} from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -10,7 +12,9 @@ const MaterialComponents = [
   MatBadgeModule,
   MatProgressSpinnerModule,
   MatToolbarModule,
-  MatSidenavModule
+  MatSidenavModule,
+  FormsModule,
+  MatMenuModule
 ];
 
 @NgModule({
